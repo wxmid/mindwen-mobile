4931693cb4791dcd306dbee0de7da754cd28f364
stuart_2 <wuxmid@163.com>
2021-08-18 09:35:17 +0800

    初始化

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React孤鸿寡鹄个
         </a>
       </header>
     </div>
